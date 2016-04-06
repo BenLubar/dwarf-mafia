806a554e84aa9f9d4024be3397d8c167f82bc6c8
Ben Lubar <ben.lubar@gmail.com>
2016-04-06 17:01:48 -0500

    more lists!

---
+++ example.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+go build && ./dwarf-mafia -seed 0 ChaosTheEternal heterodox xaade aliceif reverendryan Cheater Tsaukpaetra japonicus DoctorJones Yamikuronue fbmac abarker Captain Placeholder | sed -e 's/@/@\&zwnj;/g'
